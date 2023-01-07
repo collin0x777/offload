9e3f42871783b0fdd450cec4bac07fa75043cd6c
collin0x777 <cj.gray.713@gmail.com>
2023-01-07 17:53:05 -0500

    redo everything

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,70 @@
-use std::collections::VecDeque;
+extern crate git2;
+extern crate ssh2;
+
 use std::env;
-use std::io;
-use std::fmt;
-use std::io::Write;
-use std::str;
-// use std::fs;
-use std::collections::HashMap;
-// use ssh::*;
 use std::process::Command;
 
-// offload hostname cargo run
+use git2::{Repository, Status, StatusOptions};
+use ssh2::Session;
 
 fn main() {
-    //parse clargs
-    let mut args: VecDeque<String> = env::args().collect();
-    args.pop_front();
+    // Check the current git repository and branch
+    let repo = Repository::open(".").expect("failed to open repository");
+    let head = repo.head().expect("failed to get HEAD");
+    let branch_name = head.shorthand().expect("failed to get branch name");
+    println!("Current repository: {}", repo.path().display().to_string());
+    println!("Current branch: {}", branch_name);
 
-    if args.len() < 2 {
-        panic!("Invalid arguments: command must be of the form 'offload <hostname> <command>'")
+    // Ensure that all committed changes are pushed
+    let mut opts = StatusOptions::new();
+    opts.include_untracked(true);
+    let statuses = repo.statuses(Some(&mut opts)).expect("failed to get statuses");
+    let mut is_clean = true;
+    for entry in statuses.iter() {
+        if entry.status() != Status::CURRENT {
+            is_clean = false;
+            println!("Uncommitted changes: {}", entry.path().unwrap());
+        }
+    }
+    if !is_clean {
+        println!("There are uncommitted changes. Please commit and push them before continuing.");
+        return;
     }
 
-    let hostname = args.pop_front().unwrap();
-    let command = args.iter().fold("".to_string(), |cmd, arg| cmd + " " + arg);
+    // Get the host and command to run from command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: {} <host> <command>", args[0]);
+        return;
+    }
+    let host = &args[1];
+    let command = &args[2..];
 
-    //check git repo/branch - issue warning if there are uncommitted/unpushed changes
-    let git_branch = execute_cmd("git branch --show-current").trim().to_string();
+    // SSH into the host
+    // let tcp = std::net::TcpStream::connect(host).expect("failed to connect");
+    // let mut sess = Session::new().expect("failed to create session");
+    // sess.set_tcp_stream(tcp);
+    // sess.handshake(&tcp).expect("failed to handshake");
+    // sess.userauth_password("username", "password").expect("failed to authenticate");
 
-    //fetch remote url
-    let remote_url = execute_cmd("git config --get remote.origin.url").trim().to_string();
-    let repo_name = remote_url.trim_end_matches(".git").split("/").last().unwrap().to_string();
+    // Locate the repository on the host or clone it if it's missing
+    // let mut sftp = sess.sftp().expect("failed to start sftp session");
+    // if sftp.stat("/path/to/repo").is_err() {
+    //     println!("Repository not found on host. Cloning it now...");
+    //     let _ = Command::new("git")
+    //         .arg("clone")
+    //         .arg("/path/to/repo")
+    //         .output()
+    //         .expect("failed to execute git clone command");
+    // } else {
+    //     println!("Repository found on host.");
+    // }
 
-    println!("Hostname: {}\nCommand: {}\nBranch: {}\nRemote URL: {}\nRepository: {}", hostname, command, git_branch, remote_url, repo_name);
-
-    //connect to other computer over ssh
-
-    //find repo by remote url
-    let repo_search = execute_cmd(&format!("rg -.l --no-messages {} ~/", remote_url));
-    let mut repo_paths = repo_search.lines();
-
-    println!("{}", repo_search);
-
-    let repo_path = match repo_paths.next() {
-        Some(path) => path.to_string(),
-        None => {
-            print!("No repositories were found which track the remote, please enter a directory for the repository to be cloned:");
-        
-            let mut directory = String::new();
-            io::stdin().read_line(&mut directory)
-                .expect("Failed to read line");
-
-            execute_cmd(&format!("cd {}", directory));
-
-            execute_cmd(&format!("git clone {}", remote_url));
-
-            directory + &repo_name
-        }
-    };
-
-    //navigate to repo
-    execute_cmd(&format!("cd {}", repo_path));
-
-    //change branch
-    execute_cmd(&format!("git checkout {}", git_branch));
-
-    //pull changes
-    execute_cmd("git pull");
-
-    //run command
-    let command_output = 
-        Command::new(command)
-                .output()
-                .unwrap();
-
-    //pipe outputs
-    io::stdout().write_all(&command_output.stdout).unwrap();
-    io::stderr().write_all(&command_output.stderr).unwrap();
-
-    //pipe output files?
-}
-
-fn execute_cmd(cmd: &str) -> String {
-    let mut cmd_args = cmd.split_whitespace();
-
-    let mut command = Command::new(cmd_args.next().unwrap());
-
-    command.args(cmd_args);
-
-    let output = command.output().unwrap();
-
-    str::from_utf8(&output.stdout).unwrap().to_string()
+    // Run the specified command
+    // let output = sess.exec(command).expect("failed to execute command");
+    // let mut s = String::new();
+    // output
+    //     .read_to_string(&mut s)
+    //     .expect("failed to read output");
+    // println!("{}", s);
 }
